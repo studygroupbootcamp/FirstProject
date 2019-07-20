662e6fba6ef4664b574237f3630714ba51fa4711
Devon <Dvowen@cox.net>
2019-07-19 17:30:14 -0700

    created some skeleton

---
+++ Back End/routes/dbcommandsOne/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const route = express.Router()
+
+const CR = require('./create')
+const DL = require('./delete')
+const RT = require('./retrieve')
+const UP = require('./update')
+
+
+module.exports = routes;
